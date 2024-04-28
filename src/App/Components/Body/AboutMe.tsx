4f7be8080e422ec252b8dbefda1a96b43580b664
Sarmento Christopher <c40.sarmento@gmail.com>
2024-04-28 20:36:35 +0200

    modification du texte pour le Aboutme

--- src/App/Components/Body/AboutMe.tsx
+++ src/App/Components/Body/AboutMe.tsx
@@ -4,21 +4,34 @@ function AboutMe() {
 	return (
 		<Box>
 			<Paper elevation={3} style={{ padding: "20px", marginBottom: "20px" }}>
-				Je suis Christopher SARMENTO, actuellement en alternance chez Quest Innov. En tant qu'étudiant en 5e
-				année à l'école ESIEA Sud, je poursuis mes études dans le domaine de l'Ingénierie du Logiciel pour
-				obtenir un diplôme d'Expert en Ingénierie du Logiciel (RNCP de niveau 1), après avoir obtenu un
-				baccalauréat professionnel en Systèmes Électroniques et Numériques (SEN). J'ai eu l'opportunité
-				d'intégrer l'équipe de Quest Innov en tant que développeur en alternance, où j'ai maintenant deux ans
-				d'expérience. Ma démarche professionnelle est guidée par des valeurs telles que l'apprentissage continu,
-				la curiosité et la rigueur. C'est cette combinaison qui me motive à développer mes compétences, à
-				m'investir dans des projets personnels tout en poursuivant mes objectifs professionnels. En dehors du
-				travail, la musique occupe une place importante dans ma vie. J'apprécie une grande variété de genres
-				musicaux, que ce soit en écoutant de la Dance/Electro, du rock ou même de la hardstyle, jusqu'aux
-				classiques des années 90. Les jeux vidéo constituent également une passion pour moi. Que ce soit pour
-				explorer les aventures solo où je me plonge dans l'histoire captivante d'un personnage, ou pour exprimer
-				ma créativité à travers des jeux de construction, j'y trouve un moyen de me détendre et de m'évader. Je
-				suis particulièrement fasciné par les univers post-apocalyptiques, comme ceux que l'on retrouve dans des
-				jeux tels que Fallout, Metro ou S.T.A.L.K.E.R.
+				<Typography variant="h5" gutterBottom>
+					Qui suis-je ?
+				</Typography>
+				<Typography variant="body1" paragraph>
+					Je suis Christopher SARMENTO, actuellement en alternance chez Quest Innov. En tant qu'étudiant en 5e
+					année à l'école ESIEA Sud, je poursuis mes études dans le domaine de l'Ingénierie du Logiciel pour
+					obtenir un diplôme d'Expert en Ingénierie du Logiciel (RNCP de niveau 1), après avoir obtenu un
+					baccalauréat professionnel en Systèmes Électroniques et Numériques (SEN). J'ai eu l'opportunité
+					d'intégrer l'équipe de Quest Innov en tant que développeur en alternance, où j'ai maintenant deux
+					ans d'expérience.
+				</Typography>
+				<Typography variant="body1" paragraph>
+					Ma démarche professionnelle est guidée par des valeurs telles que l'apprentissage continu, la
+					curiosité et la rigueur. C'est cette combinaison qui me motive à développer mes compétences, à
+					m'investir dans des projets personnels tout en poursuivant mes objectifs professionnels.
+				</Typography>
+				<Typography variant="body1" paragraph>
+					En dehors du travail, la musique occupe une place importante dans ma vie. J'apprécie une grande
+					variété de genres musicaux, que ce soit en écoutant de la Dance/Electro, du rock ou même de la
+					hardstyle, jusqu'aux classiques des années 90.
+				</Typography>
+				<Typography variant="body1" paragraph>
+					Les jeux vidéo constituent également une passion pour moi. Que ce soit pour explorer les aventures
+					solo où je me plonge dans l'histoire captivante d'un personnage, ou pour exprimer ma créativité à
+					travers des jeux de construction, j'y trouve un moyen de me détendre et de m'évader. Je suis
+					particulièrement fasciné par les univers post-apocalyptiques, comme ceux que l'on retrouve dans des
+					jeux tels que Fallout, Metro ou S.T.A.L.K.E.R.
+				</Typography>
 			</Paper>
 		</Box>
 	);
