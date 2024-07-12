22d8377e500a30985a30e731465a4951718512a3
Sarmento Christopher <c40.sarmento@gmail.com>
2024-07-12 12:15:08 +0200

    Aboutme revus

--- src/App/Components/Body/AboutMe.tsx
+++ src/App/Components/Body/AboutMe.tsx
@@ -4,34 +4,65 @@ import CustomTimeLine from "./CustomTimeLine.tsx";
 function AboutMe() {
 	return (
 		<Box>
-			<Paper elevation={3} style={{ padding: "20px", marginBottom: "20px" }}>
+			<Paper
+				elevation={3}
+				style={{
+					padding: "20px",
+					marginBottom: "20px",
+					display: "flex",
+					flexDirection: "column",
+					flexWrap: "nowrap",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
 				<Typography variant="h5" gutterBottom>
 					Qui suis-je ?
 				</Typography>
 				<Typography variant="body1" paragraph>
-					Je suis Christopher SARMENTO, actuellement en alternance chez Quest Innov. En tant qu'étudiant en 5e
-					année à l'école ESIEA Sud, je poursuis mes études dans le domaine de l'Ingénierie du Logiciel pour
-					obtenir un diplôme d'Expert en Ingénierie du Logiciel (RNCP de niveau 7), après avoir obtenu un
-					baccalauréat professionnel en Systèmes Électroniques et Numériques (SEN). J'ai eu l'opportunité
-					d'intégrer l'équipe de Quest Innov en tant que développeur Full-Stack, où j'ai maintenant deux ans
-					d'expérience.
+					Ma démarche professionnelle est guidée par trois valeurs essentielles : l'apprentissage continu, la
+					curiosité et la rigueur. Je crois fermement que le développement personnel et professionnel passe
+					par une volonté constante d'apprendre et de se perfectionner. Cette philosophie m'incite à
+					m'investir pleinement dans les projets sur lesquels je travaille, tout en cherchant à atteindre mes
+					objectifs de carrière.
+				</Typography>
+				<Typography variant="h5" gutterBottom>
+					Mon projet professionnel
 				</Typography>
 				<Typography variant="body1" paragraph>
-					Ma démarche professionnelle est guidée par des valeurs telles que l'apprentissage continu, la
-					curiosité et la rigueur. C'est cette combinaison qui me motive à développer mes compétences, à
-					m'investir dans des projets personnels tout en poursuivant mes objectifs professionnels.
+					Actuellement en alternance chez Quest Innov et étudiant en cinquième année à l'école ESIEA Sud, je
+					me spécialise dans l'Ingénierie du Logiciel. Mon objectif est d'obtenir le diplôme d'Expert en
+					Ingénierie du Logiciel (RNCP de niveau 7), après avoir obtenu un baccalauréat professionnel en
+					Systèmes Électroniques et Numériques (SEN). Chez Quest Innov, en tant que développeur Full-Stack,
+					j'ai accumulé deux années d'expérience. Cette opportunité m'a permis de travailler sur des projets
+					variés et stimulants, renforçant ainsi mes compétences techniques et mon adaptabilité aux besoins du
+					marché.
+				</Typography>
+				<Typography variant="h5" gutterBottom>
+					Mes principales qualités humaines
 				</Typography>
 				<Typography variant="body1" paragraph>
-					En dehors du travail, la musique occupe une place importante dans ma vie. J'apprécie une grande
-					variété de genres musicaux, que ce soit en écoutant de la Dance/Electro, du rock ou même de la
-					hardstyle, jusqu'aux classiques des années 90.
+					Je suis quelqu'un de passionné par l'apprentissage et la découverte. Ma curiosité intellectuelle me
+					pousse à explorer constamment de nouveaux domaines et à améliorer mes compétences. La rigueur et la
+					persévérance sont également des qualités qui me définissent, me permettant de mener à bien les
+					projets auxquels je participe.
+				</Typography>
+				<Typography variant="h5" gutterBottom>
+					Mes principaux centres d'intérêt
 				</Typography>
 				<Typography variant="body1" paragraph>
-					Les jeux vidéo constituent également une passion pour moi. Que ce soit pour explorer les aventures
-					solo où je me plonge dans l'histoire captivante d'un personnage, ou pour exprimer ma créativité à
-					travers des jeux de construction, j'y trouve un moyen de me détendre et de m'évader. Je suis
+					La musique tient une place importante dans ma vie. J'apprécie une grande variété de genres musicaux,
+					allant de la Dance/Electro au rock, en passant par la hardstyle et les classiques des années 90. La
+					musique est pour moi une source d'inspiration et de relaxation, que ce soit pour se détendre après
+					une journée de travail ou pour trouver l'énergie nécessaire à la réalisation de nouveaux projets.
+				</Typography>
+				<Typography variant="body1" paragraph>
+					Les jeux vidéo constituent une autre de mes grandes passions. J'aime explorer des aventures solo
+					immersives, où je peux me plonger dans l'histoire captivante d'un personnage. De plus, les jeux de
+					construction me permettent d'exprimer ma créativité et d'explorer des mondes nouveaux. Je suis
 					particulièrement fasciné par les univers post-apocalyptiques, comme ceux que l'on retrouve dans des
-					jeux tels que Fallout, Metro ou S.T.A.L.K.E.R.
+					jeux tels que Fallout, Metro ou S.T.A.L.K.E.R. Ces jeux m'offrent un moyen de me détendre et de
+					m'évader du quotidien.
 				</Typography>
 			</Paper>
 			<Grid container sx={{ display: "flex", flexDirection: "column" }}>
